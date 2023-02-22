6243edbb62fa4287da31632fb64f8ac14cc52816
D. McDonald <d.mcdonald@MACs-MacBook-Pro.local>
2023-02-22 14:40:08 -0600

    update

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -78,7 +78,7 @@ export default function Navigation() {
       }
       accountCheck()
       status()
-  }, [accountStatus, currentUser])
+  }, [!accountStatus, currentUser])
 
     const logout = () => {
       signOut(auth)
