5657a4ce3e411f568b02b72cc69fa9564c34a815
D. McDonald <d.mcdonald@MACs-MBP.attlocal.net>
2023-02-21 21:06:30 -0600

    update

--- src/customer pages/Profile.js
+++ src/customer pages/Profile.js
@@ -69,7 +69,7 @@ export default function Profile() {
     }, [!currentUser, !docSnap, !sessionID, !stripeId])
 
         
-    if(!currentUser, !docSnap, !sessionID){
+    if(!currentUser, !sessionID){
         return(
             <Container className='page'>
                 <p>No User Logged In...</p>
