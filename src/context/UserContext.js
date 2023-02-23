913097190c1edb9fa48cd183b043c170e1dd588d
D. McDonald <d.mcdonald@MACs-MBP.attlocal.net>
2023-02-23 00:10:36 -0600

    update

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -37,7 +37,6 @@ export function UserContextProvider({ children }) {
             });
         retrieveUser()
         retrieveAccountDetails()
-        return unsubscribe
     }, [])
 
     // login user
