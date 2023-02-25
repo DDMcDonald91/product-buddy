919ab388c7132abfca36c873078e313523be8bfc
D. McDonald <d.mcdonald@MACs-MBP.attlocal.net>
2023-02-25 01:18:00 -0600

    update

--- src/assets/components/Renew.js
+++ src/assets/components/Renew.js
@@ -33,7 +33,7 @@ export default function Renew() {
         }
         accountCheck()
 
-    }, [currentUser, docSnap, !stripeId])
+    }, [currentUser, !docSnap, !stripeId])
 
   return (
     <Container className='page'>
