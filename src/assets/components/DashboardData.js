025c773f0ff52c40a600741d1e7204e8e5008023
D. McDonald <d.mcdonald@MACs-MBP.attlocal.net>
2023-03-13 21:19:31 -0500

    update

--- src/assets/components/DashboardData.js
+++ src/assets/components/DashboardData.js
@@ -1,73 +1,73 @@
 export const DashboardData = [
     {
         title:"Product Description Template",
-        description:"This is a tool used to create creative SEO friendly descriptions for your ecommerce products.",
+        description:"Create compelling, SEO-friendly product descriptions to help your products stand out from the competition.",
         link:"/product-description-template",
         color:"success"
     },
     {
         title:"Product Title Template",
-        description:"Need a name for your products? Start here!",
+        description:"Get inspired and come up with catchy product names that capture your audience's attention.",
         link:"/product-title-template",
         color:"success"
     },
     {
         title:"Business Name Template",
-        description:"Need a name for your new business? Start here!",
+        description:"Struggling to come up with a name for your business? Let this tool help you generate unique and memorable names.",
         link:"/business-name-template",
         color:"success"
     },
     {
         title:"Business Slogan Template",
-        description:"This is a tool used to create a creative slogan for your business.",
+        description:"Craft a catchy slogan that communicates your brand's message and sets you apart from your competitors.",
         link:"/business-slogan-template",
         color:"success"
     },
     {
         title:"AI Art Template",
-        description:"Create AI generated art with the click of a button!",
+        description:"Let the power of AI create unique, eye-catching art for your website or social media.",
         link:"/image-template",
         color:"secondary"
     },
     {
         title:"Social Media Post Template",
-        description:"Start right here to make a unique social media post!",
+        description:"Make your social media posts stand out with creative templates that grab your audience's attention.",
         link:"/social-media-post-template",
         color:"danger"
     },
     {
         title:"Social Media Content Format Template",
-        description:"Start here to create a format for your conent!",
+        description:"Ensure your social media content looks professional and on-brand with customizable content formats.",
         link:"/social-media-content-format-template",
         color:"danger"
     },
     {
         title:"Social Media Strategy Template",
-        description:"Need a strategy for your social media brand? Start here!",
+        description:"Develop a winning social media strategy that helps you grow your audience and drive engagement.",
         link:"/social-media-strategy-template",
         color:"danger"
     },
     {
         title:"Youtube Video Script Template",
-        description:"Need a script for your youtube video? Start here!",
+        description:"Create a compelling script that captures your audience's attention and keeps them engaged throughout your video.",
         link:"/youtube-script-template",
         color:"danger"
     },
     {
         title:"Youtube Video Title Template",
-        description:"Need a clever title for your youtube video? Start here!",
+        description:"Come up with click-worthy titles that entice your audience to watch your videos and boost your views.",
         link:"/youtube-title-template",
         color:"danger"
     },
     {
         title:"Youtube Video Descriptions Template",
-        description:"Need a unique description for your youtube video? Start here!",
+        description:"Generate engaging video descriptions that help your videos rank higher in search results and attract more views.",
         link:"/youtube-description-template",
         color:"danger"
     },
     {
         title:"Youtube Video Topics Template",
-        description:"Need some ideas for your youtube video? Start here!",
+        description:"Get inspired and come up with fresh and unique video ideas that will keep your audience coming back for more.",
         link:"/youtube-topic-template",
         color:"danger"
     },
