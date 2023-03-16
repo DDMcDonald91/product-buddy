56066d54577d58cede9e489f1125a511706a7b58
D. McDonald <d.mcdonald@MACs-MBP.attlocal.net>
2023-03-15 21:08:34 -0500

    update

--- src/customer pages/Checkout.js
+++ src/customer pages/Checkout.js
@@ -16,7 +16,7 @@ export default function Checkout() {
         } else {
           accountUpdate();
         }
-      }, [docSnap.customerData.id, currentUser]);
+      }, [!docSnap.customerData.id, currentUser]);
       
 
       const accountUpdate = async () => {
